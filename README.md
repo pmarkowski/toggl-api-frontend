084f8dd376e20ce5dd44dba03d0591a50bc5473a
Philip Markowski <philipmarkowski642@gmail.com>
2016-10-11 22:18:22 -0400

    Initial commit, basic inputs and start of scripts set up

---
+++ README.md
@@ -0,0 +1,3 @@
+# Toggl API Front End
+
+The purpose of this project is to make entering multiple dates with identical data into Toggl more easily.
---
+++ script.js
@@ -0,0 +1,17 @@
+var user, password;
+
+var togglApi = "https://www.toggl.com/api/v8/";
+
+function authenticate() {
+    var user = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', togglApi + "/me");
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            alert(xhr.response.since);
+        }
+    };
+    xhr.send();
+}
