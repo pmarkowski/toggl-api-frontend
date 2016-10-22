5d6a6d7344a3639903604fbb86d387283d6a12fb
Philip Markowski <philipmarkowski642@gmail.com>
2016-10-22 13:26:21 -0400

    Slight corrections in the script

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var user, password;
 
-var togglApi = "https://www.toggl.com/api/v8/";
+var togglApi = "https://www.toggl.com/api/v8";
 
 function authenticate() {
     var user = document.getElementById('username').value;
@@ -8,6 +8,7 @@ function authenticate() {
 
     var xhr = new XMLHttpRequest();
     xhr.open('GET', togglApi + "/me");
+    xhr.setRequestHeader('Authorization', 'Basic ' + btoa(user + ':' + password));
     xhr.onload = function() {
         if (xhr.status === 200) {
             alert(xhr.response.since);
