99468aaa8d46604444b0a079db348c9f2e0bc49d
Philip Markowski <philipmarkowski642@gmail.com>
2016-10-23 09:52:33 -0400

    At a functional state

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ var user, password;
 
 var togglApi = "https://www.toggl.com/api/v8";
 
+var millisecondsPerDay = 24 * 60 * 60 * 1000;
+var secondsPerHour = 60 * 60;
+
 window.addEventListener('load', initialize);
 
 // Set up event listeners
@@ -10,6 +13,7 @@ function initialize() {
     document.getElementById('authenticate').addEventListener('click', authenticate);
     document.getElementById('workspace').addEventListener('change', workspaceChanged);
     document.getElementById('project').addEventListener('change', projectChanged);
+    document.getElementById('submit-time-entries').addEventListener('click', createTimeEntries);
 }
 
 // Functions
@@ -38,6 +42,28 @@ function togglGet(user, password, endpoint, success) {
     }
 
     xhr.send();
+}
+
+function togglJsonPost(user, password, endpoint, data, success) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', togglApi + endpoint);
+    xhr.setRequestHeader('Authorization', 'Basic ' + btoa(user + ':' + password));
+    xhr.setRequestHeader('Content-Type', 'application/json');
+
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            var response = JSON.parse(xhr.response);
+            if (response) {
+                if (success) {
+                    success(response);
+                }
+            }
+        } else {
+            error('Error calling Toggl API!');
+        }
+    }
+
+    xhr.send(JSON.stringify(data));
 }
 
 function authenticate() {
@@ -100,5 +126,62 @@ function populateTasks(tasks) {
 
     for (var i = 0; i < tasks.length; i++) {
         addOption(tasksDropdown, tasks[i].id, tasks[i].name);
+    }
+}
+
+function dateFromDateAndTimeInputStrings(date, time) {
+    var year = date.substring(0, 4);
+    // Subtract one since JS months are 0 indexed
+    var month = date.substring(5, 7) - 1;
+    var date = date.substring(8, 10);
+    var hours = time.substring(0, 2);
+    var minutes = time.substring(3, 5);
+
+    return new Date(year, month, date, hours, minutes);
+}
+
+function createTimeEntries() {
+    var taskId = parseInt(document.getElementById('task').value);
+    var description = document.getElementById('description').value;
+    var billable = document.getElementById('billable').value === 'true';
+    var startTime = document.getElementById('start-time').value;
+    var durationH = document.getElementById('duration').value;
+    // var endTime = document.getElementById('end-time').value;
+    var startDateString = document.getElementById('start-date').value;
+    var endDateString = document.getElementById('end-date').value;
+
+    var startDate, endDate;
+
+    startDate = dateFromDateAndTimeInputStrings(startDateString, startTime);
+
+    if (endDateString) {
+        endDate = dateFromDateAndTimeInputStrings(endDateString, startTime);
+    }
+
+    var numDayEntries;
+    if (endDate) {
+        numDayEntries = (endDate - startDate) / millisecondsPerDay;
+    } else {
+        numDayEntries = 1;
+    }
+
+    var postObject = {
+        "time_entry": {
+            "description": description,
+            "tid": taskId,
+            "billable": billable,
+            "start": null,
+            "duration": durationH * secondsPerHour,
+            "created_with": 'Toggl API Frontend'
+        }
+    };
+
+    for (var i = 0; i <= numDayEntries; i++) {
+        var postDate = new Date(startDate);
+        postDate.setDate(startDate.getDate() + i);
+
+        postObject.time_entry.start = postDate.toISOString();
+
+        togglJsonPost(user, password, '/time_entries', postObject);
     }
 }
